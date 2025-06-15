6d17b544f829f6e3f92352a30243af06d4110f6c
Ulric Wilfred <shaman.sir@gmail.com>
2025-06-15 15:48:36 +0200

    drafting `Tree` component

--- src/Component/Tree.purs
+++ src/Component/Tree.purs
@@ -1,6 +1,43 @@
-module Component.Tree where
+module Yoga.Tree.Svg.Component.Tree where
 
 import Prelude
 
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+
+import Yoga.Tree (Tree)
+-- import Yoga.Tree.Zipper (Path)
+
 
 foo = 42
+
+data Action = Increment | Decrement
+
+
+type State a =
+    { tree :: Tree a
+    , focus :: Unit
+    }
+
+
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
+  where
+  initialState _ = 0
+
+  render state =
+    HH.div_
+      [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
+      , HH.div_ [ HH.text $ show state ]
+      , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
+      ]
+
+  handleAction = case _ of
+    Increment -> H.modify_ \state -> state + 1
+    Decrement -> H.modify_ \state -> state - 1
--- src/Demo.purs
+++ src/Demo.purs
@@ -9,6 +9,8 @@ import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 
+import Yoga.Tree.Svg.Component.Tree as YogaSvgTree
+
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
