2e03582cadb70d44b416e56c50bcde16707e1bc7
Ulric Wilfred <shaman.sir@gmail.com>
2025-06-16 17:24:21 +0200

    fixing `Tree` component to work with `Tree`s

--- src/Component/Tree.purs
+++ src/Component/Tree.purs
@@ -2,26 +2,36 @@ module Yoga.Tree.Svg.Component.Tree where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
+import Data.Foldable (foldl)
+import Data.Traversable (traverse)
+
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 
 import Yoga.Tree (Tree)
+import Yoga.Tree as Tree
+import Yoga.Tree.Extended.Path (Path)
+import Yoga.Tree.Extended.Path (advance, up, find, root, traverse) as Path
 -- import Yoga.Tree.Zipper (Path)
 
 
 foo = 42
 
-data Action = Increment | Decrement
+data Action
+    = Advance Int
+    | GoUp
 
 
 type State a =
     { tree :: Tree a
-    , focus :: Unit
+    , focus :: Path
     }
 
 
+component ∷ ∀ query action output m. H.Component query action output m
 component =
   H.mkComponent
     { initialState
@@ -29,15 +39,21 @@ component =
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
   where
-  initialState _ = 0
+  initialState _ =
+    { tree : Tree.mkLeaf unit
+    , focus : Path.root
+    }
 
   render state =
-    HH.div_
-      [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
-      , HH.div_ [ HH.text $ show state ]
-      , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
-      ]
+    case Path.find state.focus state.tree of
+        Just tree -> HH.text "some tree"
+            -- foldl ?wh ?wh tree
+        Nothing -> HH.text "*"
+    -- HH.div_
+    --   [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
+    --   , HH.div_ [ HH.text $ show state ]
+    --   , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
+    --   ]
 
   handleAction = case _ of
-    Increment -> H.modify_ \state -> state + 1
-    Decrement -> H.modify_ \state -> state - 1
+    _ -> pure unit
