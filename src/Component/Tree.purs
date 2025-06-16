bbe59313026991e52cc3405f9a0a8d2a803f651e
Ulric Wilfred <shaman.sir@gmail.com>
2025-06-16 23:10:35 +0200

    rendering actual tree

--- src/Component/Tree.purs
+++ src/Component/Tree.purs
@@ -3,26 +3,35 @@ module Yoga.Tree.Svg.Component.Tree where
 import Prelude
 
 import Data.Maybe (Maybe(..))
-import Data.Foldable (foldl)
+import Data.Foldable (foldl, foldr)
 import Data.Traversable (traverse)
+import Data.Tuple (uncurry)
+import Data.Tuple.Nested ((/\), type (/\))
+import Data.Array ((:))
+import Data.Array (snoc) as Array
 
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 import Halogen.HTML.Events as HE
 
 import Yoga.Tree (Tree)
 import Yoga.Tree as Tree
+import Yoga.Tree.Extended as Tree
 import Yoga.Tree.Extended.Path (Path)
-import Yoga.Tree.Extended.Path (advance, up, find, root, traverse) as Path
+import Yoga.Tree.Extended.Path (advance, up, find, root, traverse, fill) as Path
 -- import Yoga.Tree.Zipper (Path)
 
-
-foo = 42
 
 data Action
     = Advance Int
     | GoUp
+
+
+type Input a =
+    { tree :: Tree a
+    }
 
 
 type State a =
@@ -31,7 +40,7 @@ type State a =
     }
 
 
-component ∷ ∀ query action output m. H.Component query action output m
+component :: forall a query output m. Show a => H.Component query (Input a) output m
 component =
   H.mkComponent
     { initialState
@@ -39,16 +48,24 @@ component =
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
   where
-  initialState _ =
-    { tree : Tree.mkLeaf unit
-    , focus : Path.root
-    }
+  initialState { tree } =
+    { tree, focus : Path.root }
 
   render state =
-    case Path.find state.focus state.tree of
-        Just tree -> HH.text "some tree"
-            -- foldl ?wh ?wh tree
+    case Path.find state.focus $ Path.fill state.tree of
+        Just tree ->
+            renderNode tree
         Nothing -> HH.text "*"
+
+  renderNode node =
+    HH.div
+        [ HP.style "padding-left: 10px;" ]
+        $ (HH.text $ show $ Tree.value node)
+        : (foldl Array.snoc []
+             $ renderNode
+            <$> Tree.children node
+          )
+
     -- HH.div_
     --   [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
     --   , HH.div_ [ HH.text $ show state ]
--- src/Demo.purs
+++ src/Demo.purs
@@ -9,6 +9,10 @@ import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 
+import Type.Proxy (Proxy(..))
+
+import Yoga.Tree (Tree)
+import Yoga.Tree as Tree
 import Yoga.Tree.Svg.Component.Tree as YogaSvgTree
 
 main :: Effect Unit
@@ -16,8 +20,47 @@ main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI component unit body
 
-data Action = Increment | Decrement
 
+type Slots =
+  ( tree :: forall q o. H.Slot q o Unit
+  )
+
+
+type State a =
+  { tree :: Tree a
+  }
+
+
+_tree  = Proxy :: _ "tree"
+
+
+data Action
+  = Initialize
+
+
+type Item = Int
+
+
+myTree :: Tree Item
+myTree =
+  Tree.mkTree 1
+    [ Tree.mkLeaf 11
+    , Tree.mkTree 12
+      [ Tree.mkTree 121 [ Tree.mkLeaf 1211, Tree.mkLeaf 1212 ]
+      , Tree.mkTree 122 [ Tree.mkLeaf 1221, Tree.mkLeaf 1222, Tree.mkLeaf 1223 ]
+      , Tree.mkLeaf 123
+      , Tree.mkTree 124 [ Tree.mkLeaf 1241 ]
+      , Tree.mkLeaf 125
+      ]
+    , Tree.mkTree 13 [ Tree.mkLeaf 131, Tree.mkLeaf 132, Tree.mkLeaf 133, Tree.mkLeaf 134, Tree.mkLeaf 135, Tree.mkLeaf 136, Tree.mkLeaf 137 ]
+    , Tree.mkLeaf 14
+    , Tree.mkLeaf 15
+    , Tree.mkTree 16
+      [ Tree.mkLeaf 161 ]
+    ]
+
+
+component ∷ ∀ query input output m. H.Component query input output m
 component =
   H.mkComponent
     { initialState
@@ -25,15 +68,11 @@ component =
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
   where
-  initialState _ = 0
+  initialState :: input -> State Item
+  initialState _ = { tree : myTree }
 
+  render :: forall action. State Item -> H.ComponentHTML action Slots m
   render state =
-    HH.div_
-      [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
-      , HH.div_ [ HH.text $ show state ]
-      , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
-      ]
+    HH.slot_ _tree unit YogaSvgTree.component { tree : state.tree }
 
-  handleAction = case _ of
-    Increment -> H.modify_ \state -> state + 1
-    Decrement -> H.modify_ \state -> state - 1
+  handleAction = const $ pure unit
